aff5f758e33e87b0cdf5c2f53dfa9eb4df2e3547
AdrianLeonRivera <56595479+AdrianLeonRivera@users.noreply.github.com>
2021-11-16 10:35:41 +0100

    Subida desafio elasticsearchlogstash

---
+++ elasticsearchlogstash/src/main/java/com/bootcamp/elasticsearchlogstash/ElasticsearchlogstashApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.elasticsearchlogstash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchlogstashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticsearchlogstashApplication.class, args);
+	}
+
+}
---
+++ elasticsearchlogstash/src/test/java/com/bootcamp/elasticsearchlogstash/ElasticsearchlogstashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.elasticsearchlogstash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchlogstashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
